7eed6d5c218deb1cf7dfdaa08aad1e4ceb77b501
Daniel Meechan <git@public.dmeechan.com>
2021-10-24 19:16:49 +0100

    Read video frames

--- src/run.py
+++ src/run.py
@@ -1,4 +1,57 @@
+import os
+import sys
+
+import cv2
+
 def start():
-    print("Hello from start")
+    num_args = len(sys.argv)
+    if num_args != 3:
+        raise Exception(
+            f"Found {num_args} args - expected 3: COMMAND <video name> <video path>"
+        )
+
+    _, name, path = sys.argv
+    read_frames(name, path)
+
+
+def read_frames(name, path):
+    # Opens the Video file
+    # Sauce: https://learnopencv.com/reading-and-writing-videos-using-opencv/#read-video-from-file
+    video = cv2.VideoCapture(path)
+    time = 0
+
+    if video.isOpened() is False:
+        raise Exception("Error opening the video file")
+
+    stats = {
+        "width": video.get(cv2.CAP_PROP_FRAME_WIDTH),
+        "height": video.get(cv2.CAP_PROP_FRAME_HEIGHT),
+        "fps": video.get(cv2.CAP_PROP_FPS),
+        "frame_count": video.get(cv2.CAP_PROP_FRAME_COUNT),
+    }
+    print(f"Found video {name} with stats:", stats)
+
+    print("Creating folder")
+    if not os.path.exists(name):
+        os.makedirs(name)
+
+    print("Reading video")
+    while video.isOpened():
+        ret, frame = video.read()
+        if ret == False:
+            break
+
+        if time == 0:
+            print("Found first frame")
+
+        image_path = f"{name}/frame-{str(time)}.jpg"
+        # cv2.imwrite(image_path, frame)
+        time += 1
+
+    print("Done!")
+    video.release()
+    cv2.destroyAllWindows()
+
 
 start()
+ 
